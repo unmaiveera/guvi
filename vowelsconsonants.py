de6633487bd228f1d7bdef11aa238b8202eb4886
unmaiveera <50871362+unmaiveera@users.noreply.github.com>
2019-05-25 09:46:36 -0700

    Create vowelsconsonants.py

---
+++ vowelsconsonants.py
@@ -0,0 +1,7 @@
+ch = input("")
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+  print("Vowel")
+elif(ch=='b' or ch=='c'or ch=='d'or ch=='f'or ch=='g'or ch=='h'or ch=='j'or ch=='k'or ch=='l'or ch=='m'or ch=='n'or ch=='p'or ch=='q'or ch=='r'or ch=='s'or ch=='t'or ch=='v'or ch=='w' or ch=='x'or ch=='y'or ch=='z'):
+  print("Consonant")
+else:
+  print("invalid")
